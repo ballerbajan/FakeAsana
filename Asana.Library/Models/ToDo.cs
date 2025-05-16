88d52f05e061e0e579aa42cb1d87ec9cb29cb0a4
Isiaq Allison <ballerbajan@gmail.com>
2025-05-16 10:39:55 -0400

    Add project files.

---
+++ Asana.Library/Models/ToDo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Library.Models
+{
+    public class Todo
+    {
+        public string name {
+            get {
+                return name;
+            }
+            set
+            {
+                if (name != value) { 
+                    name = value;
+                }
+            }
+        }
+        public string description;
+        public bool isDone;
+        public int priority;
+
+        public Todo()
+        {
+            name = "test";
+        }
+      
+
+    }
+
+}
---
+++ Asana.cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using Asana.Library.Models;
+using System;
+
+namespace Asana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ToDo Myfirsttodo = new ToDo();
+        }
+    }
+}
